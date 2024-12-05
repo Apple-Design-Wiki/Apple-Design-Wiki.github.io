07b35a6520845909dffcda037e748c211e6cce6d
WRXinYue <wrxinyue@proton.me>
2024-12-05 23:29:52 +0800

    feat: align the layout with apple developer guidelines #29

--- components/ValaxyMain.vue
+++ components/ValaxyMain.vue
@@ -22,46 +22,46 @@ onMounted(() => {
 </script>
 
 <template>
-  <main flex="~" class="oceanus-valaxy-main">
+  <main flex="~" class="oceanus-valaxy-main apple-container">
     <div>
       <div class="pt-[16px]" style="top: var(--oceanus-c-nav-height); width: var(--oceanus-c-aside-width);">
         <img :src="frontmatter.image as string || 'https://valaxy-theme-oceanus.vercel.app/assets/0311imac_20-zwr3Vp6a.png'">
       </div>
     </div>
 
-    <div w="full" flex="~">
-      <slot name="main">
-        <div class="content" flex="~ col grow" w="full">
-          <slot name="main-header" />
-          <slot name="main-header-after" />
+    <slot name="main">
+      <slot name="main-header" />
+      <slot name="main-header-after" />
 
-          <slot name="main-content">
-            <div class="oceanus-container prose">
-              <ValaxyMd :frontmatter="frontmatter">
-                <slot name="main-content-md" />
-                <slot />
-              </ValaxyMd>
-            </div>
-            <slot name="main-content-after" />
-          </slot>
-        </div>
-
-        <slot name="main-nav-before" />
-
-        <slot name="main-nav" />
-
-        <slot name="main-nav-after" />
-
-        <slot v-if="siteConfig.comment.enable && frontmatter.comment !== false" name="comment" />
-
-        <slot name="footer" />
+      <slot name="main-content">
+        <ValaxyMd :frontmatter="frontmatter" class="min-w-0">
+          <div class="max-w-none prose">
+            <slot name="main-content-md" />
+          </div>
+          <slot />
+          <slot name="main-content-after" />
+        </ValaxyMd>
       </slot>
-    </div>
 
-    <slot name="aside">
-      <OceanusToc v-if="aside">
-        <slot name="aside-custom" />
-      </OceanusToc>
+      <slot name="main-nav-before" />
+
+      <slot name="main-nav" />
+
+      <slot name="main-nav-after" />
+
+      <slot v-if="siteConfig.comment.enable && frontmatter.comment !== false" name="comment" />
+
+      <slot name="footer" />
     </slot>
+
+    <OceanusToc v-if="aside" class="apple-toc">
+      <slot name="aside-custom" />
+    </OceanusToc>
   </main>
 </template>
+
+<style lang="scss" scoped>
+.apple-container {
+  width: 980px;
+}
+</style>
