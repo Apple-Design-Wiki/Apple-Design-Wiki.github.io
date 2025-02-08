9317c06d3cb5f69a28e9d044024d8e1694c80888
WRXinYue <wrxinyue@proton.me>
2025-02-09 03:09:12 +0800

    chore: change ImageGrid name to ImgGallery

--- components/ImgGallery.vue
+++ components/ImgGallery.vue
@@ -88,7 +88,7 @@ watchEffect(async () => {
     </article>
   </section>
 
-  <section v-else-if="mode === 'grid'" class="grid-gallery gallery" grid="~ cols-1 lg:cols-2 xl:cols-3" style="gap: var(--gap);" :style="{ '--min-height': minHeight, '--max-height': maxHeight }">
+  <section v-else-if="mode === 'grid'" class="gallery grid-gallery" grid="~ cols-1 lg:cols-2 xl:cols-3" style="gap: var(--gap);" :style="{ '--min-height': minHeight, '--max-height': maxHeight }">
     <article v-for="images, idx of parts" :key="idx" flex="~ col" style="gap: var(--gap);">
       <img v-for="image, i of images" :key="i" w="full" h="full" :src="image" alt="image" :style="{ '--enter-stage': idx + 1 }" class="slide-enter gallery-item object-cover">
       <!-- TODO: expandable -->
